0d12f907fc11669df17705f7401feeb091c87f40
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-17 10:55:44 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "techopsbucket123"
+    key            = "vpcec2/terraform.tfstate8"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "demo_table123"
+  }
+}
