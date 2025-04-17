ff11b56f29b2b13b0ca18d1efda158045693314f
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-17 21:36:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
-provider "aws" {
-  region = var.region
+
+terraform {
+  required_version = ">= 1.10.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.87.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = var.aws_region
 
 data "aws_availability_zones" "available" {}
 
