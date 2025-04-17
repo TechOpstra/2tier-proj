d279071068eb7cf1f883d93e973c02fc59e616db
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-17 21:50:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "rds" {
   db_name               = var.db_name
   username              = var.db_username
   password              = var.db_password
-  parameter_group_name  = "default.mysql8.0"
+  #parameter_group_name  = "default.mysql8.0"
   vpc_security_group_ids = [module.security_groups.db_sg_id]
   tags                  = var.tags
 }
